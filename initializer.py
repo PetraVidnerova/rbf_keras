372da364c1ebbdd2872470411585e9d9edbbe823
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 10:15:53 -0700

    Fixed PYTHON_W291 issues in initializer.py

--- initializer.py
+++ initializer.py
@@ -1,20 +1,20 @@
 from keras.initializers import Initializer
-import numpy as np 
+import numpy as np
 
 class InitFromFile(Initializer):
     """ Initialize the weights by loading from file.
 
     # Arguments
-        filename: name of file, should by .npy file 
+        filename: name of file, should by .npy file
     """
     def __init__(self, filename):
-        self.filename = filename 
+        self.filename = filename
 
     def __call__(self, shape, dtype=None):
         with open(self.filename, "rb") as f:
             X = np.load(f)
-        assert shape == X.shape 
-        return X 
+        assert shape == X.shape
+        return X
 
     def get_config(self):
         return {
